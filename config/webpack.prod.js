56b0871657fb8d530d06b2f2e06855fb391f9531
vldmr <thewumin@gmail.com>
2020-04-03 20:58:39 +0300

    add uglify js for prod webpack;

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,40 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common')
+const merge = require("webpack-merge");
+const common = require("./webpack.common");
+
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 module.exports = merge(common, {
-  mode: 'production',
-})
+  mode: "production",
+
+  plugins: [new CompressionWebpackPlugin()],
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        sourceMap: false,
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: {
+            unsafe: true,
+            inline: true,
+            passes: 2,
+            keep_fargs: false,
+          },
+          output: {
+            beautify: false,
+          },
+          mangle: true,
+        },
+      }),
+      // new OptimizeCSSPlugin({
+      //   cssProcessorOptions: {
+      //     "preset": "advanced",
+      //     "safe": true,
+      //     "map": { "inline": false },
+      //   },
+      // }),
+    ],
+  },
+});
