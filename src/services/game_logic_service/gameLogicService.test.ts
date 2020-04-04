b83bc31dba7d7cca28e8af0b6635fd28905bc6d9
vldmr <thewumin@gmail.com>
2020-04-04 16:23:40 +0300

    game logic: init and move tiles;

--- src/services/game_logic_service/gameLogicService.test.ts
+++ src/services/game_logic_service/gameLogicService.test.ts
@@ -1,3 +1,101 @@
-test("test jest", () => {
-  expect(1).toBe(1);
+import GameLogicService, { MoveDirection } from "./gameLogicService";
+
+describe("Game logic service", () => {
+  test("initState should init state", () => {
+    const initialState = GameLogicService.gameStateInit();
+
+    expect(initialState.isGameOver).toBe(false);
+    expect(initialState.isWin).toBe(false);
+    expect(initialState.score).toBe(0);
+    expect(initialState.board.map(({ value }) => value)).toEqual(
+      GameLogicService._boardInit().map(({ value }) => value)
+    );
+  });
+
+  test("addTile should add new tile", () => {
+    const initialState = GameLogicService.gameStateInit();
+
+    const newState = GameLogicService.addTile(initialState);
+
+    expect(newState.board.filter(({ value }) => value === 2).length).toBe(1);
+  });
+
+  test("move: should shift tiles and merge with move UP", () => {
+    const initState = GameLogicService.gameStateInit();
+
+    initState.board[4].value = 4;
+    initState.board[8].value = 8;
+    initState.board[12].value = 8;
+
+    initState.board[10].value = 4;
+    initState.board[14].value = 4;
+
+    const resultState = GameLogicService.move(initState, MoveDirection.UP);
+
+    expect(resultState.board[0].value).toBe(4);
+    expect(resultState.board[4].value).toBe(16);
+
+    expect(resultState.board[2].value).toBe(8);
+
+    expect(resultState.score).toBe(24);
+  });
+
+  test("move: should shift tiles and merge with move DOWN", () => {
+    const initState = GameLogicService.gameStateInit();
+
+    initState.board[4].value = 4;
+    initState.board[8].value = 8;
+    initState.board[12].value = 8;
+
+    initState.board[6].value = 4;
+    initState.board[14].value = 4;
+
+    const resultState = GameLogicService.move(initState, MoveDirection.DOWN);
+
+    expect(resultState.board[8].value).toBe(4);
+    expect(resultState.board[12].value).toBe(16);
+
+    expect(resultState.board[14].value).toBe(8);
+
+    expect(resultState.score).toBe(24);
+  });
+
+  test("move: should shift tiles and merge with move RIGHT", () => {
+    const initState = GameLogicService.gameStateInit();
+
+    initState.board[0].value = 16;
+    initState.board[1].value = 16;
+    initState.board[3].value = 16;
+
+    initState.board[8].value = 4;
+    initState.board[10].value = 4;
+
+    const resultState = GameLogicService.move(initState, MoveDirection.RIGHT);
+
+    expect(resultState.board[2].value).toBe(16);
+    expect(resultState.board[3].value).toBe(32);
+
+    expect(resultState.board[11].value).toBe(8);
+
+    expect(resultState.score).toBe(40);
+  });
+
+  test("move: should shift tiles and merge with move LEFT", () => {
+    const initState = GameLogicService.gameStateInit();
+    initState.board[0].value = 8;
+    initState.board[1].value = 8;
+    initState.board[3].value = 16;
+
+    initState.board[8].value = 4;
+    initState.board[10].value = 4;
+
+    const resultState = GameLogicService.move(initState, MoveDirection.LEFT);
+
+    expect(resultState.board[0].value).toBe(16);
+    expect(resultState.board[1].value).toBe(16);
+
+    expect(resultState.board[8].value).toBe(8);
+
+    expect(resultState.score).toBe(24);
+  });
 });
--- src/services/game_logic_service/gameLogicService.ts
+++ src/services/game_logic_service/gameLogicService.ts
@@ -1,7 +1,180 @@
+import cloneDeep from "lodash/cloneDeep";
+// import isEqual from 'lodash/isEqual';
+import range from "lodash/range";
+
+type IBoardTile = {
+  value: number;
+  id: string;
+};
+
+type IBoard = IBoardTile[];
+
+type IGameState = {
+  board: IBoard;
+  isGameOver: boolean;
+  isWin: boolean;
+  score: number;
+};
+
+export enum MoveDirection {
+  UP = 1,
+  DOWN,
+  LEFT,
+  RIGHT,
+}
+
 class GameLogicService {
   static _currentId: number = 1;
-  static generateId(): number {
-    return this._currentId++;
+  static generateId(): string {
+    return (this._currentId++).toString();
+  }
+
+  static _boardInit(): IBoard {
+    return Array(16)
+      .fill(null)
+      .map(() => ({
+        value: 0,
+        id: this.generateId(),
+      }));
+  }
+
+  static gameStateInit(): IGameState {
+    return {
+      board: this._boardInit(),
+      isGameOver: false,
+      isWin: false,
+      score: 0,
+    };
+  }
+
+  static addTile(gameState: IGameState): IGameState {
+    const emptyTileIndexes = gameState.board
+      .map(({ value }, index) => (value === 0 ? index : -1))
+      .filter((index) => index >= 0);
+
+    if (emptyTileIndexes.length === 0) return;
+
+    const randomIndex =
+      emptyTileIndexes[Math.floor(Math.random() * emptyTileIndexes.length)];
+
+    gameState.board[randomIndex].value = 2;
+
+    return { ...gameState };
+  }
+
+  static _getRevercedRowIndexes(
+    moveDirection: MoveDirection,
+    row: number
+  ): number[] {
+    if (moveDirection === MoveDirection.RIGHT) {
+      return range(1, 5)
+        .map((i) => 4 * row + i - 1)
+        .reverse();
+    }
+
+    if (moveDirection === MoveDirection.LEFT) {
+      return range(1, 5).map((i) => 4 * row + i - 1);
+    }
+
+    if (moveDirection === MoveDirection.DOWN) {
+      return range(0, 4)
+        .map((i) => 4 * i + row)
+        .reverse();
+    }
+
+    if (moveDirection === MoveDirection.UP) {
+      return range(0, 4).map((i) => 4 * i + row);
+    }
+
+    return [];
+  }
+
+  static _mergeBoard(state: IGameState, indexes: number[]): IGameState {
+    const board = state.board;
+
+    let currentTileIndex: number | null = null;
+    let scoreDelta = 0;
+    for (const index of indexes) {
+      if (board[index].value === 0) {
+        continue;
+      }
+
+      if (
+        currentTileIndex !== null &&
+        board[index].value === board[currentTileIndex].value
+      ) {
+        const tmp = board[currentTileIndex];
+        board[currentTileIndex] = board[index];
+        board[index] = tmp;
+
+        board[currentTileIndex].value *= 2;
+        scoreDelta += board[currentTileIndex].value;
+        board[index].value = 0;
+
+        currentTileIndex = null;
+        continue;
+      }
+
+      if (board[index].value !== 0) {
+        currentTileIndex = index;
+      }
+    }
+
+    return {
+      ...state,
+      board,
+      score: state.score + scoreDelta,
+    };
+  }
+
+  static _shiftBoard(state: IGameState, indexes: number[]): IGameState {
+    const board = state.board;
+
+    let pivot = 0;
+    while (pivot <= 3) {
+      if (board[indexes[pivot]].value !== 0) {
+        pivot++;
+        continue;
+      }
+
+      let currentTileIndex: number | null = null;
+      for (const index of indexes.slice(pivot, indexes.length)) {
+        if (currentTileIndex === null) {
+          currentTileIndex = index;
+          continue;
+        }
+
+        if (board[index].value !== 0) {
+          const tmp = board[currentTileIndex];
+          board[currentTileIndex] = board[index];
+          board[index] = tmp;
+          currentTileIndex = index;
+        }
+      }
+
+      pivot++;
+    }
+
+    return {
+      ...state,
+      board,
+    };
+  }
+
+  static move(
+    currentState: IGameState,
+    moveDirection: MoveDirection
+  ): IGameState {
+    let newState = cloneDeep(currentState);
+
+    for (const row of range(0, 4)) {
+      const indexes = this._getRevercedRowIndexes(moveDirection, row);
+
+      newState = this._mergeBoard(newState, indexes);
+      newState = this._shiftBoard(newState, indexes);
+    }
+
+    return newState;
   }
 }
 
